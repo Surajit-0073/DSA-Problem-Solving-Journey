be1cf836d963f50003e947a1a4c4f74a83c287a9
Surajit Bera <sbera1875@gmail.com>
2026-07-02 13:44:27 +0530

    Time: 22 ms (53%), Space: 33.4 MB (39.29%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,36 +1,28 @@
-const int N=5000;
-int q[N], front, back;// implement deque
-int maxH[N];
-int d[5]={0, 1, 0, -1, 0};
 class Solution {
 public:
-    static inline int idx(int i, int j, int c){ return i*c+j; }
-    static bool outSide(int i, int j, int r, int c){
-        return i<0||i>=r||j<0||j>=c;
-    }
-    static bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        const int r=grid.size(), c=grid[0].size();
-        memset(maxH, -1, r*c*sizeof(int));
-        front=back=N/2;// reset q
-        q[back++]=0; 
-        maxH[0]=health-grid[0][0];
-        while(front<back){
-            int ij=q[front++];// pop front
-            int curH=maxH[ij];
-            if (ij==r*c-1) return curH>0;
-            auto [i, j]=div(ij, c);
-            for(int a=0; a<4; a++){
-                const int s=i+d[a], t=j+d[a+1];
-                if (outSide(s, t, r, c)) continue;
-                const int st=idx(s,t, c);
-                int H2=curH-grid[s][t];
-                if(H2>maxH[st]){
-                    maxH[st]=H2;
-                    if (grid[s][t]==0) q[--front]=st;
-                    else q[back++]=st;
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int m=grid.size(),n=grid[0].size();
+        vector<vector<int>>dist(m,vector<int>(n,INT_MIN));
+        queue<pair<int,pair<int,int>>>q;
+        q.push({health-grid[0][0],{0,0}});
+        dist[0][0]=grid[0][0];
+        int dx[4]={0,0,1,-1};
+        int dy[4]={1,-1,0,0};
+        while(!q.empty()){
+           int h=q.front().first,i=q.front().second.first,j=q.front().second.second;
+           q.pop();
+           if(h<dist[i][j])continue;
+           for(int k=0;k<4;k++){
+            int ni=i+dx[k],nj=j+dy[k];
+            if(ni>=0 && ni<m && nj>=0 && nj<n){
+                int nh = h-grid[ni][nj];
+                if(nh>dist[ni][nj]){
+                    dist[ni][nj]=nh;
+                    q.push({nh,{ni,nj}});
                 }
             }
+           }
         }
-        return 0;
+        return dist[m-1][n-1]>=1;
     }
 };
